6cc12aee7aae0eda41db89a4228e8777aca05545
alanthomas5226 <masto5226spro@gmail.com>
2024-05-04 12:42:25 -0500

    se añade carpeta de Js/

---
+++ js/ejemplo.js
@@ -0,0 +1,25 @@
+function calculate() {
+    const number1 = document.querySelector('#number1').valueAsNumber
+    const number2 = document.querySelector('#number2').valueAsNumber
+    const operation = document.querySelector('#operation').value
+    let result = 0
+    
+
+
+    switch (operation) {
+        //parseInt --> para cambiar de texto a numero
+        //valvueAsNumber --> se pone decimal al numero
+        case '+':
+            result = number1 + number2
+            break;
+        case '-':
+            result = number1 - number2
+            break;
+    }
+
+const divResult = document.querySelector('#result')
+divResult.innerHTML = result
+
+
+console.log(number1, number2, operation, result)
+}
